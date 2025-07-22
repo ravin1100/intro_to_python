9ba0bd807d82d1548e35e07765d96a0203ba18e4
ravin1100 <ravinjangir.rj1100@gmail.com>
2025-07-22 18:09:14 +0530

    Personal Information Collector

---
+++ personal_information_collector.py
@@ -0,0 +1,10 @@
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+city = input("Enter Your City: ")
+hobby = input("Enter Your Hobby: ")
+
+print(f"Hello, {name}!\n")
+print(f"Your are {age} years old and live in {city}.")
+print(f"In your free time, you enjoy {hobby}.")
+
+
